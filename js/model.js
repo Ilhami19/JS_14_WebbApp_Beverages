682a0544e68c59f78a65d5b51a0d41670ff38dd9
Peter <schmidt.multimedia@gmail.com>
2021-05-28 10:24:02 +0200

    Model / Data / GUI

--- js/model.js
+++ js/model.js
@@ -1,5 +1,17 @@
 /* model (Daten/Logik) */
 
+let data = {
+    milk:{lower:0, upper:5, bev:"milch"},
+    juice:{lower:6, upper:12, bev:"saft"},
+    cola:{lower:13, upper:20, bev:"cola"},
+    wine:{lower:21, upper:110, bev:"wein"},
+    default:{bev:"tee"}
+}
+
+let gui =   {
+            img:{path:"./bilder/", ext:".jpg"} 
+        }
+
 
 
 
